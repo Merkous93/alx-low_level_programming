4329b4a2f2d87185d92692cf644acd27acd19913
your name <omar.merkous@gmail.com>
2023-04-10 07:08:11 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-gcc -c *.c
-ar rcs liball.a *.o
+
+# create_static_lib.sh
+
+# Create an object file for each .c file in the current directory
+for cfile in *.c; do
+    gcc -c -Wall -Werror -Wextra -pedantic "$cfile"
+done
+
+# Create the static library from all the .o files
+ar -rc liball.a *.o
+
+# Clean up the .o files
+rm -f *.o
+
+# Print the contents of the static library
+echo "Static library 'liball.a' created with the following contents:"
+ar -t liball.a
+
