88be181f6513c91959452a20cdaa921ee0495208
Merkous93 <omar.merkous@gmail.com>
2023-07-04 16:18:57 -0700

    Den Rest

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
- * print_listint - check the code
- * @h: a
- * Return: Always 0.
+ * print_listint - function that prints all the elements of a listint_t list
+ * @h: pointer to the node head
+ * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *nav = h;
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
+
 /**
- * listint_len - check the code
- * @h: a
+ * sum_listint - check the code
+ * @head: a
  * Return: Always 0.
  */
-size_t listint_len(const listint_t *h)
+int sum_listint(listint_t *head)
 {
-	const listint_t *nav = h;
+	listint_t *nav = head;
 	size_t count = 0;
 
 	while (nav)
 	{
-		count++;
+		count += nav->n;
 		nav = nav->next;
 	}
 	return (count);
